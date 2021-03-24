e224bd79a8403b59ab124438a8216cf4e54836bb
Igor Toporkov <iigortoporkov@gmail.com>
2021-03-24 21:27:30 +0100

    Implement Shameless

--- lib/shameless.ex
+++ lib/shameless.ex
@@ -1,18 +1,10 @@
 defmodule Shameless do
-  @moduledoc """
-  Documentation for `Shameless`.
-  """
+  import Plug.Conn
 
-  @doc """
-  Hello world.
+  @spec init(any) :: any
+  def init(options), do: options
 
-  ## Examples
-
-      iex> Shameless.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def call(conn, _) do
+    Map.put(conn, :shameless_plug, "Check out my github: https://github.com/4tyTwo")
   end
 end
